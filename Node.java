6f7229d01dc541668e84ff3b9dd9ae3e0dd12623
Chaotic-V01D <bmyoungbauer@gmail.com>
2026-04-29 19:01:58 -0500

    Added basic functionality to Nodes

--- Node.java
+++ Node.java
@@ -1,6 +1,24 @@
-class Node {
-    String word;
-    int[] idx = new int[3];
+public class Node {
+    int nodeIndex;
+    String[] words = new String[3];
     Node[] children = new Node[3];
     boolean isLeaf;
+
+    public Node(String word, int nodeIndex) {
+        this.words[0] = word;
+        this.nodeIndex = nodeIndex;
+    }
+
+    public Node(String[] words, int nodeIndex) {
+        this.words = words;
+        this.nodeIndex = nodeIndex;
+    }
+
+    public String[] getVal(){
+        return this.words;
+    }
+
+    public Node[] getChildren(){
+        return this.children;
+    }
 }
