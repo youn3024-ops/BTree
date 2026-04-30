8e8a6ce21e512f55844f702c268286d2c9fff3aa
Chaotic-V01D <bmyoungbauer@gmail.com>
2026-04-29 19:17:27 -0500

    Implemented basic B-Tree search

--- Node.java
+++ Node.java
@@ -21,4 +21,20 @@ public class Node {
     public Node[] getChildren(){
         return this.children;
     }
+
+
+    //https://www.reddit.com/r/computerscience/comments/1sukkgh/made_a_diagram_to_finally_understand_btree/
+    public boolean search(String value){
+        int i = 0;
+        while (this.words[i].compareTo(value)<0){
+            i++;
+        }
+        if (this.words[i].equals(value)){
+            return true;
+        }
+        if (this.isLeaf){
+            return false;
+        }
+        return this.children[i].search(value);
+    }
 }
